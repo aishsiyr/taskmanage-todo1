be3d60072339c100e8424cfdf50e7f3cd3b16fc1
Aishsiyr <aishwaryasathish22@gmail.com>
2023-10-25 10:18:07 +0530

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import Modal from "react-modal";
